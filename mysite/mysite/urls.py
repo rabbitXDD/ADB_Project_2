28d6426b81ea6a214be1e3aece81aed0f2f78482
李明緯 <rockmanx41024@gmail.com>
2017-11-14 22:53:51 +0800

    first commit

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,36 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+import Iuno
+
+from django.conf.urls import include, url, patterns
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+]
+
+# put this before django-cms urls, 
+# see https://stackoverflow.com/questions/22705939/
+# django-debug-toolbar-shows-up-but-the-panels-return-404
+if settings.DEBUG:
+    Iuno.attachDebugURLs(settings, urlpatterns)
+
+# Django CMS
+urlpatterns += patterns('',
+        url(r'', include('cms.urls'))
+)
